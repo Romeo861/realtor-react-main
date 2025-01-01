6f3be3a87a53ca7b2e8f140c639f0f95e6dd0d84
Ramy Moussa <104394939+Romeo861@users.noreply.github.com>
2025-01-01 14:24:17 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./Header.scss";
+import { Link, NavLink } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const Header = () => {
+  const [pageState, setPageState] = useState({
+    path: "sign-in",
+    menuName: "Sign in",
+  });
+  const { path, menuName } = pageState;
+
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState({
+          path: "profile",
+          menuName: "Profile",
+        });
+      } else {
+        setPageState({
+          path: "sign-in",
+          menuName: "Sign in",
+        });
+      }
+    });
+  }, [auth]);
+
+  return (
+    <div className='header'>
+      <header className='header__container'>
+        <Link to='/' className='header__logo'>
+          <h1 className='header__title'>Code Solution 365</h1>
+        </Link>
+        <nav>
+          <ul className='header__menus'>
+            <NavLink to='/' className='header__menu'>
+              Home
+            </NavLink>
+
+            <NavLink to='/offers' className='header__menu'>
+              Offers
+            </NavLink>
+
+            <NavLink to={`/${path}`} className='header__menu'>
+              {menuName}
+            </NavLink>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
